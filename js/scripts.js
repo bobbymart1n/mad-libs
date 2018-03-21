5b1855a97e2778c78d3276ad85effaceb64812e8
Bobby Martin and Cameron Button <cameron@>
2018-03-21 09:17:43 -0700

    hook inputs up to submit button

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
-  $("$formOne").submit(function(event){
-    
+  $("#formOne").submit(function(event){
+    var input1 = $('#input1').val();
+    var input2 = $('#input2').val();
+    var input3 = $('#input3').val();
+    var input4 = $('#input4').val();
+    var input5 = $('#input5').val();
+    $('.person1').text(input1);
+    $('.person2').text(input2);
+    $('.animal').text(input3);
+    $('.exclamation').text(input4);
+    $('.verb').text(input5);
+
+    $('#story').show();
     event.preventDefault();
   });
 });
