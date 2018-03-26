a850065c026f05b67b2594e2159ea63eda1602df
Guest User <Guest@Epicodus-9B.local>
2018-03-26 09:24:11 -0700

    Refactor JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,11 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event){
-    var input1 = $('#input1').val();
-    var input2 = $('#input2').val();
-    var input3 = $('#input3').val();
-    var input4 = $('#input4').val();
-    var input5 = $('#input5').val();
-    $('.person1').text(input1);
-    $('.person2').text(input2);
-    $('.animal').text(input3);
-    $('.exclamation').text(input4);
-    $('.verb').text(input5);
+    var blanks = ["input1", "input2", "input3", "input4", "input5"];
+
+    blanks.forEach(function(blank) {
+      var userInput = $("#" + blank).val();
+      $("." + blank).text(userInput);
+    });
 
     $('#story').show();
     event.preventDefault();
